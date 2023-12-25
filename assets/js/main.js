2ffbcb29f2e7783172a07ddb36466d6e97e16044
Fathullah Qiscus <93090495+fathullahqiscus@users.noreply.github.com>
2023-12-25 21:56:07 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,18 +39,19 @@ var x = setInterval(function() {
     var now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    var distance = now - countDownDate;
 
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365.25));
+  var months = Math.floor((distance % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("days").innerHTML = years;
+    document.getElementById("hours").innerHTML = months;
+    document.getElementById("minutes").innerHTML = days;
 
     // If the count down is over, write some text
     if (distance < 0) {
